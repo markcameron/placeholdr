34a0e653554af609a2c7957f8f3cdaa61750ed0a
Mark Cameron <mark.cameron@devfactory.ch>
2015-09-08 11:17:20 +0200

    Serve images set by size in url {width}/{height}

---
+++ app/Http/Controllers/PlaceholdersController.php
@@ -0,0 +1,40 @@
+<?php namespace App\Http\Controllers;
+
+use Intervention\Image\ImageManager;
+
+class PlaceholdersController extends Controller {
+
+  protected $image_manager;
+
+  protected $format;
+
+  public function __construct() {
+    $this->image_manager = new ImageManager();
+    $this->format = 'png';
+  }
+
+  /**
+   * Show the profile for the given user.
+   *
+   * @param  int  $id
+   * @return Response
+   */
+  public function widthHeight($width, $height) {
+    $image = $this->image_manager->canvas($width, $height);
+
+    $image->text($width .'x'. $height, $width / 2, $height / 2, function($font) {
+      $font->file(base_path() . '/resources/fonts/OpenSans-Regular.ttf');
+      $font->size(24);
+      $font->color('#000000');
+      $font->align('center');
+      $font->valign('center');
+      $font->angle(0);
+    });
+
+		$response = response()->make($image->encode($this->format), 200);
+		$response->header('Content-Type', 'image/'. $this->format);
+
+    return $response;
+  }
+
+}
