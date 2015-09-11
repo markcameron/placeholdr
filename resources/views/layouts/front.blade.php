4160347515213c32223e6f3e5cfcb12a5d3a8bbf
Mark Cameron <mark.cameron@devfactory.ch>
2015-09-11 11:37:40 +0200

    ANALYTICS: Moved code to the top of the page

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -35,6 +35,16 @@
 
   <body data-spy="scroll" data-offset="20" data-target="#navbar">
 
+    <script>
+     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+                              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+     ga('create', 'UA-67364324-1', 'auto');
+     ga('send', 'pageview');
+    </script>
+
     <div class="logo-menu">
       <nav class="navbar navbar-default navbar-fixed-top" role="navigation" data-spy="affix" data-offset-top="50">
         <div class="container">
@@ -95,16 +105,6 @@
     <!-- Copyright Section End-->
 
     <script src="/assets/js/prism.js"></script>
-
-    <script>
-     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-       (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-                              m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-                              })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-     ga('create', 'UA-67364324-1', 'auto');
-     ga('send', 'pageview');
-    </script>
 
   </body>
 </html>
